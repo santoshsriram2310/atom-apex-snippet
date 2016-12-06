e6d9542b4b936b86e7ac59ce9e477d60d6095e3b
Santosh Kumar Sriram <santosh.2310@gmail.com>
2016-12-06 15:32:46 -0800

    FirstCommit

---
+++ spec/atom-snippets-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomSnippetsView from '../lib/atom-snippets-view';
+
+describe('AtomSnippetsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
